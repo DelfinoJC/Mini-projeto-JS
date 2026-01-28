9016a43064b9ddad8a4e97339d096d3860a379d3
DelfinoJC <136389693+DelfinoJC@users.noreply.github.com>
2026-01-27 22:38:24 -0300

    feat: adding opening and closing of modal

---
+++ scprit.js
@@ -0,0 +1,9 @@
+const newTaskButton = () => {
+    overlay.classList.add('active');
+    createTask.classList.add('active');
+}
+
+const closeModal = () => {
+    overlay.classList.remove('active');
+    createTask.classList.remove('active');
+}
