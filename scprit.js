142d3f90717d1c9c15fee9ec13afb49e97ff4e94
DelfinoJC <136389693+DelfinoJC@users.noreply.github.com>
2026-01-29 22:15:54 -0300

    feat: adding search and data insertion functions

--- scprit.js
+++ scprit.js
@@ -7,3 +7,31 @@ const closeModal = () => {
     overlay.classList.remove('active');
     createTask.classList.remove('active');
 }
+
+const getTask = () => {
+    fetch('http://localhost:3000/task')
+    .then(res => res.json())
+    .then(data => {
+        insertTask(data)
+    })
+}
+
+getTask();
+
+const insertTask = (taskList) => {
+    if (taskList.length > 0) {
+        listTasks.innerHTML = '';
+        taskList.map(task => {
+            listTasks.innerHTML +=
+                `<li>
+                <h3>${task.title}</h3>
+                <p>${task.description}</p>
+                <div class="actions">
+                    <img src="./images/edit.png" alt="editar" onclick="editTask(this)">
+                    <img src="./images/trash.png" alt="lixeira" onclick="deleteTask(this)">
+                </div>
+            </li>
+            `;
+        });
+    }
+}
