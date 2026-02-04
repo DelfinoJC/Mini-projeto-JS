eafe8d818993dee573b357ae55f899f308776172
DelfinoJC <136389693+DelfinoJC@users.noreply.github.com>
2026-02-03 23:31:15 -0300

    feat: Add functionality to search for tasks by description and title.

--- scprit.js
+++ scprit.js
@@ -28,7 +28,7 @@ const insertTask = (taskList) => {
                 <p>${task.description}</p>
                 <div class="actions">
                     <img src="./images/edit.png" alt="editar" onclick="editTask(this)">
-                    <img src="./images/trash.png" alt="lixeira" onclick="deleteTask(this)">
+                    <img src="./images/trash.png" alt="lixeira" onclick="deleteTask(${task.id})">
                 </div>
             </li>
             `;
@@ -52,6 +52,38 @@ const createTaskFunction = () => {
         .then(res => res.json())
         .then(data => {
             getTask();
+            let form = document.querySelector('#createTask form');
+            form.reset();
         })
     closeModal();
+}
+
+
+
+const deleteTask = (id) => {
+    fetch(`http://localhost:3000/task/${id}`, {
+        method: 'DELETE'
+    })
+        .then(res => res.json())
+        .then(data => {
+            getTask();
+        })
+};
+
+const exportTask = () => {
+    console.log(getTaskHtml.value);
+    let tasks = document.querySelectorAll('ul li');
+    if (getTaskHtml.value.length > 0) {
+        tasks.forEach(task => {
+            if (!task.children[1].innerText.includes(getTaskHtml.value) && !task.children[0].innerText.includes(getTaskHtml.value)) {
+                task.classList.add('hidden');
+            } else {
+                task.classList.remove('hidden');
+            }
+        });
+    } else {
+        tasks.forEach(task => {
+            task.classList.remove('hidden');
+        });
+    }
 }
